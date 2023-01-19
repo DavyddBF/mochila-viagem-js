7e30b59cd240e745026beb702338baf0fb5c326e
Davyd Ferreira <100801538+DavyddBF@users.noreply.github.com>
2023-01-19 15:14:05 -0300

    Refatoração

--- main.js
+++ main.js
@@ -3,7 +3,11 @@ const form = document.getElementById("novoItem")
 const lista = document.getElementById("lista")
 
 // array que receberá palavras escritas pelo usuário no input
-const itens = []
+const itens = JSON.parse(localStorage.getItem("itens")) || []
+
+itens.forEach( (elemento) => {
+    criaElemento(elemento)
+});
 
 // cria um evento quando clicado o botão de submit
 form.addEventListener("submit", (evento) => {
@@ -13,14 +17,25 @@ form.addEventListener("submit", (evento) => {
     const nome = evento.target.elements['nome']
     const quantidade = evento.target.elements['quantidade']
 
-    criaElemento(nome.value, quantidade.value)
+    // itemAtual - variável para tranformar o valor do input num objeto. onde nome, após os dois pontos(:) é o parametro de valor recebido, onde o usuário adiciona uma palavra
+    const itemAtual = {
+    "nome": nome.value,
+    "quantidade": quantidade.value
+    }
+
+    criaElemento(itemAtual)
+    // empurra o itemAtual e seus objetos para o array
+    itens.push(itemAtual)
+
+    // Adicionando item e transformando ele numa string por meio do JSON.stringify()
+    localStorage.setItem("itens", JSON.stringify(itens))
     
     // deixa os inputs vazios
     nome.value = ""
     quantidade.value = ""
 })
 
-function criaElemento(nome, quantidade) {
+function criaElemento(item) {
 
     // cria uma li e adiciona a classe "item"
     const novoItem = document.createElement('li')
@@ -28,23 +43,11 @@ function criaElemento(nome, quantidade) {
 
     // cria um strong, pega o strong e modifica ele adicionando o parametro quantidade
     const numeroItem = document.createElement('strong')
-    numeroItem.innerHTML = quantidade
+    numeroItem.innerHTML = item.quantidade
 
     // pega a li e coloca como filho dentro da li o strong, modifica o strong e após ele adiciona o parametro nome
     novoItem.appendChild(numeroItem)
-    novoItem.innerHTML += nome
+    novoItem.innerHTML += item.nome
 
     lista.appendChild(novoItem)
-
-    // itemAtual - variável para tranformar o valor do input num objeto. onde nome, após os dois pontos(:) é o parametro de valor recebido, onde o usuário adiciona uma palavra
-    const itemAtual = {
-        "nome": nome,
-        "quantidade": quantidade
-    }
-
-    // empurra o itemAtual e seus objetos para o array
-    itens.push(itemAtual)
-
-    // Adicionando item e transformando ele numa string por meio do JSON.stringify()
-    localStorage.setItem("item", JSON.stringify(itens))
 }
